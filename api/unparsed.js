31d0dcf8f7e8ed2525b6e522a07fda53ad772235
sungjoo-XCENA <sungjoo.ha@xcena.com>
2026-05-16 19:09:03 +0900

    Remove unused review endpoint

--- api/unparsed.js
+++
@@ -1,9 +0,0 @@
-module.exports = function handler(_req, res) {
-  res.setHeader("cache-control", "no-store");
-  res.status(200).json({
-    count: 0,
-    items: [],
-    listReviewCount: 0,
-    priceReviewCount: 0,
-  });
-};
